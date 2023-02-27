77f6c046b22f51d8ef66a12321c8af1f1bb4c433
eblunck <emilia.blunck@gmail.com>
2023-02-27 14:33:44 +0100

    add movie

--- src/app/jennifer-data-service.service.ts
+++ src/app/jennifer-data-service.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { ViewMovieComponent as Movie } from './view-movie/view-movie.component';
 import { BehaviorSubject } from 'rxjs';
 
-
 export const TEST_BOOKS: Movie[] = [
   { name: "A", desc: "A Book", author: "Alle", id: "0", review: "Bra" },
   { name: "B", desc: "B Book", author: "Blle", id: "1", review: "Dålig" }
@@ -18,8 +17,10 @@ export class JenniferDataServiceService {
 
   watchList$ = this.moviesToWatch.asObservable();
 
-  setWatchList() {
-    console.log("setting watch-list to: " + TEST_BOOKS.map(movie => movie.name))
+  setWatchList(movies: Movie[]) {
+    this.moviesToWatch.next(movies);
+  }
+  setInitialList() {
     this.moviesToWatch.next(TEST_BOOKS);
   }
 }
--- src/app/view-movie-list/view-movie-list.component.ts
+++ src/app/view-movie-list/view-movie-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {JenniferDataServiceService} from '../jennifer-data-service.service';
+import { ViewMovieComponent as Movie } from '../view-movie/view-movie.component';
 
 @Component({
   selector: 'app-view-movie-list',
@@ -8,12 +9,16 @@ import {JenniferDataServiceService} from '../jennifer-data-service.service';
 })
 export class ViewMovieListComponent {
   constructor(public data: JenniferDataServiceService) {
-
+  this.data.setInitialList();
+  }
+  showMovies() : string {
+    return JSON.stringify(this.data['moviesToWatch']['_value']);
   }
 
-  showMovies() : string {
-    this.data.setWatchList();
-    console.log(this.data['moviesToWatch']['_value']);
-    return JSON.stringify(this.data['moviesToWatch']['_value']);
+  addMovie() {
+    let hej: Movie[] = [this.data['moviesToWatch']['_value'],
+      { name: "C", desc: "A Book", author: "Calle", id: "2", review: "Mjeh" }];
+    this.data.setWatchList(hej);
+    console.log("hej");
   }
 }
