328dddf559005433b8b8fda5d536aa43c6700504
eblunck <emilia.blunck@gmail.com>
2023-03-01 10:53:56 +0100

    router typ

--- src/app/view-movie/view-movie.component.ts
+++ src/app/view-movie/view-movie.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-movie',
   templateUrl: './view-movie.component.html',
   styleUrls: ['./view-movie.component.css']
 })
-export class ViewMovieComponent {
-  name:string = "";
-  desc:string = "";
-  author:string = "";
-  id:string = "";
-  review:string = "";
+export class ViewMovieComponent implements OnInit {
+
+  id: number | undefined;
+  
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = +params['id']; 
+    });
+  }
 }
