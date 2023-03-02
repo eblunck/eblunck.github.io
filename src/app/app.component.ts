ccc109e795c89ee395610563075bf83dc3b59cc8
MajaRygard <maja.rygard@gmail.com>
2023-03-02 13:38:10 +0100

    snygga knappar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { JenniferDataServiceService } from './jennifer-data-service.service';
 import { HttpClient } from '@angular/common/http';
 
+
 const url: string =
   'https://api.themoviedb.org/3/discover/movie?api_key=e6171b13d4159aa39793cc0b447bbb93&with_cast=4491';
 
